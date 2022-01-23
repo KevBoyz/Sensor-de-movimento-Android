9792f42ddf9267b8801172b9138cbd63c1dcbe7e
KevBoyz <Kevinho_gameplays@hotmail.com>
2022-01-23 17:29:39 -0300

    Simple events

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 from kivymd.app import MDApp
 from kivymd.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.dialog import MDDialog
+import webbrowser
+
+
+def make_invisible(widget):
+    widget.visible = False
+    widget.size_hint_x = None
+    widget.size_hint_y = None
+    widget.height = 0
+    widget.width = 0
+    widget.text = ""
+    widget.opacity = 0
 
 
 class Root(FloatLayout):
-    ...
+    def hyperlink(self, url):
+        webbrowser.open(url)
+
+
+class Menu(FloatLayout):
+    dialog = None
+
+    def HideMenuSection(self):
+        make_invisible(self)
+
+    def open_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Manual de instruções",
+                type="custom",
+                content_cls=HelpContent()
+            )
+        self.dialog.open()
+
+
+class HelpContent(BoxLayout):
+    pass
+
 
 
 class MainApp(MDApp):
